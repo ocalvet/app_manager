b7d0b95a8e3dc81a65c8a95448506b849dd8cdbe
Ovidio R. Calvet <calveto@gmail.com>
2019-03-22 17:50:01 -0400

    Switch to axios

--- src/services/Applications.js
+++ src/services/Applications.js
@@ -1,19 +1,18 @@
+const axios = require('axios');
+
 class Applications {
   constructor(auth) {
     this.auth = auth;
   }
   getAll = async () => {
     try {
-      const response = await fetch(
-        `${process.env.REACT_APP_API_URL}/applications`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${this.auth.getUser().token}`
-          }
+      const response = await axios({
+        url: `${process.env.REACT_APP_API_URL}/applications`,
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${this.auth.getUser().token}`
         }
-      );
+      });
       const json = await response.json();
       return json.data;
     } catch (e) {
--- src/services/Authenticator.js
+++ src/services/Authenticator.js
@@ -1,19 +1,19 @@
+const axios = require('axios');
+
 class Authenticator {
   authenticate = async (username, password) => {
     try {
-      const response = await fetch(
-        `${process.env.REACT_APP_API_URL}/auth/signin`,
-        {
-          headers: new Headers({
-            'Content-Type': 'application/json'
-          }),
-          method: 'POST',
-          body: JSON.stringify({
-            username,
-            password
-          })
-        }
-      );
+      const response = await axios({
+        url: `${process.env.REACT_APP_API_URL}/auth/signin`,
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        }),
+        method: 'POST',
+        body: JSON.stringify({
+          username,
+          password
+        })
+      });
       if (response.status !== 200) throw new Error('Error in response');
       const json = await response.json();
       localStorage.setItem('user', JSON.stringify(json));
