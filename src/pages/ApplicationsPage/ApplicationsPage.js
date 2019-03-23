1169cd8e5e5e0d3b713e45237b816951ae568ac5
Ovidio R. Calvet <calveto@gmail.com>
2019-03-23 09:31:44 -0400

    Remove title

--- src/pages/ApplicationsPage/ApplicationsPage.js
+++ src/pages/ApplicationsPage/ApplicationsPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import ApplicationIcon from './components/ApplicationIcon';
 import { requestApplications, selectApplication } from '../../actions';
 import history from '../../history';
@@ -27,9 +26,6 @@ class ApplicationsPage extends React.Component {
     const { applications, classes } = this.props;
     return (
       <div>
-        <Typography variant="h4" gutterBottom component="h2">
-          Applications Page
-        </Typography>
         <div className={classes.applicationsContainer}>
           {applications.map((app, i) => (
             <ApplicationIcon key={i} application={app} onSelectApplication={this.selectApplication} />
